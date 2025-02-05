4a89902a9518a6962d3a0c1f6923c78feba974c9
avv <avv37@yandex.com>
2025-02-05 17:39:55 +0300

    Вторая доработка финального проекта спринта 4 по замечаниям.

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -60,12 +60,9 @@ public class TaskManager {
 
     public int createTask(Task task) {
         if (task != null) {
-            int uid = task.getUid();
-            if (uid == 0) {
-                uid = getUidCounter();
-                task.setUid(uid);
-                tasks.put(uid, task);
-            }
+            int uid = getUidCounter();
+            task.setUid(uid);
+            tasks.put(uid, task);
             return uid;
         }
         return 0;
@@ -73,12 +70,9 @@ public class TaskManager {
 
     public int createEpic(Epic epic) {
         if (epic != null) {
-            int uid = epic.getUid();
-            if (uid == 0) {
-                uid = getUidCounter();
-                epic.setUid(uid);
-                epics.put(uid, epic);
-            }
+            int uid = getUidCounter();
+            epic.setUid(uid);
+            epics.put(uid, epic);
             return uid;
         }
         return 0;
@@ -87,20 +81,15 @@ public class TaskManager {
     public int createSubtask(Subtask subtask) {
         if (subtask != null) {
             int epicUid = subtask.getEpicUid();
-            Epic epic = getEpicById(epicUid);
+            Epic epic = epics.get(epicUid);
             if (epic != null) {
-                if (epics.containsKey(epic.getUid())) {
-                    int uid = subtask.getUid();
-                    if (uid == 0) {
-                        uid = getUidCounter();
-                        subtask.setUid(uid);
-                        subtasks.put(uid, subtask);
-                        epic.getSubtasks().add(subtask);
-                        epic.changeStatusDependingOnSubtasks();
-                        epics.put(epicUid, epic);
-                    }
-                    return uid;
-                }
+                int uid = getUidCounter();
+                subtask.setUid(uid);
+                subtasks.put(uid, subtask);
+                epic.getSubtasks().add(subtask);
+                epic.changeStatusDependingOnSubtasks();
+                epics.put(epicUid, epic);
+                return uid;
             }
         }
         return 0;
@@ -113,8 +102,12 @@ public class TaskManager {
     }
 
     public void updateEpic(Epic epic) {
-        if (epics.containsKey(epic.getUid())) {
-            epics.put(epic.getUid(), epic);
+        int epicId = epic.getUid();
+        if (epics.containsKey(epicId)) {
+            Epic epic1 = epics.get(epicId);
+            epic1.setDescription(epic.getDescription());
+            epic1.setName(epic.getName());
+            epics.put(epicId, epic1);
         }
     }
 
@@ -123,7 +116,8 @@ public class TaskManager {
         if (subtasks.containsKey(subtaskId)) {
             Subtask oldSubtask = subtasks.get(subtaskId);
             subtasks.put(subtaskId, subtask);
-            Epic epic = getEpicById(oldSubtask.getEpicUid());
+            Epic epic = epics.get(oldSubtask.getEpicUid());
+            //Epic epic = getEpicById(oldSubtask.getEpicUid());
             if (epic != null) {
                 epic.getSubtasks().remove(oldSubtask);
                 epic.getSubtasks().add(subtask);
@@ -143,7 +137,8 @@ public class TaskManager {
 
     public void deleteEpicById(int uid) {
         if (epics.containsKey(uid)) {
-            Epic epic = getEpicById(uid);
+            //Epic epic = getEpicById(uid);
+            Epic epic = epics.get(uid);
             if (epic != null) {
                 ArrayList<Subtask> epicSubtasks = epic.getSubtasks();
                 for (Subtask subtask : epicSubtasks) {
@@ -156,20 +151,24 @@ public class TaskManager {
     }
 
     public void deleteAllEpics() {
-        deleteAllSubtasks();
+        //deleteAllSubtasks();
+        subtasks.clear();
         epics.clear();
     }
 
     public void deleteSubtaskById(int uid) {
         if (subtasks.containsKey(uid)) {
-            Subtask subtask = getSubtaskById(uid);
+            //Subtask subtask = getSubtaskById(uid);
+            Subtask subtask = subtasks.get(uid);
             if (subtask != null) {
-                Epic epic = getEpicById(subtask.getEpicUid());
+                //Epic epic = getEpicById(subtask.getEpicUid());
+                Epic epic = epics.get(subtask.getEpicUid());
                 if (epic != null) {
                     subtasks.remove(uid);
                     epic.getSubtasks().remove(subtask);
                     epic.changeStatusDependingOnSubtasks();
-                    updateEpic(epic);
+                    epics.put(epic.getUid(), epic);
+                    //updateEpic(epic);
                 }
             }
         }

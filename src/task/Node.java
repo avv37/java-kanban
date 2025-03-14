5fab8727e63863a801578d223d2e4fa3a8a02f75
avv <avv37@yandex.com>
2025-03-14 17:56:42 +0300

    Исправление CheckStyle

--- src/task/Node.java
+++ src/task/Node.java
@@ -5,6 +5,7 @@ public class Node {
     private final Task data;
     private Node next;
     private Node prev;
+
     public Node(Node prev, Task data, Node next) {
         this.data = data;
         this.next = next;
